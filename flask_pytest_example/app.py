4de98e737d5eb6f61b8704584dfbf07960f18285
ZhiruC <zhiruchen@hotmail.fr>
2022-05-31 10:00:24 +0000

    rajout CSRFProtect

--- flask_pytest_example/app.py
+++ flask_pytest_example/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from flask_pytest_example.handlers.routes import configure_routes
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 configure_routes(app)
 
