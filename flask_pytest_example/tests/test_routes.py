4f779484c2ca1a3081826e789499d371674a9791
ZhiruC <zhiruchen@hotmail.fr>
2022-05-31 10:25:29 +0000

    add csrf

--- flask_pytest_example/tests/test_routes.py
+++ flask_pytest_example/tests/test_routes.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-csrf = CSRFProtect(app)
 import json
 
 from flask_pytest_example.handlers.routes import configure_routes
@@ -8,6 +7,8 @@ from flask_pytest_example.handlers.routes import configure_routes
 
 def test_base_route():
     app = Flask(__name__)
+    csrf = CSRFProtect(app)
+    csrf.init_app(app)
     configure_routes(app)
     client = app.test_client()
     url = '/'
