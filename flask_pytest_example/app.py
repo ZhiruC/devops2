7a696fefdb4c24d6fbcb1a1e1a6e9488aec1445b
ZhiruC <zhiruchen@hotmail.fr>
2022-05-31 12:07:00 +0000

    rm flask_pytest_exemple

--- flask_pytest_example/app.py
+++ flask_pytest_example/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-from flask_pytest_example.handlers.routes import configure_routes
+from handlers.routes import configure_routes
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
