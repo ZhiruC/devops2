df328a72f6aefee963fb1b3266c174c096f899f3
ZhiruC <zhiruchen@hotmail.fr>
2022-06-01 12:57:56 +0000

    add compose

--- flask_pytest_example/app.py
+++ flask_pytest_example/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+from redis import Redis
 from handlers.routes import configure_routes
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 csrf.init_app(app)
+redis = Redis(host='redis', port=6379)
 
 configure_routes(app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
